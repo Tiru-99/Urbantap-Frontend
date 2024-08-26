1edbad99d9201913b4eb3acec83dbe7e7eded094
Kartik Pal <kartik.p1110@gmail.com>
2024-08-26 22:09:41 +0530

    feat: added initial UI

---
+++ src/components/CardsSection.tsx
@@ -0,0 +1,12 @@
+import UrbantapLogo from '@/assets/logo';
+import React from 'react';
+
+const CardsSection: React.FC = () => {
+  return (
+    <div className='h-[1152px] bg-white'>
+        <UrbantapLogo />
+    </div>
+  );
+};
+
+export default CardsSection;
---
+++ src/components/ReviewsSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewsSection: React.FC = () => {
+  return (
+    <div className='h-[850px] bg-gray-100'>
+      <h2 className='text-black'>Reviews Section</h2>
+    </div>
+  );
+};
+
+export default ReviewsSection;
